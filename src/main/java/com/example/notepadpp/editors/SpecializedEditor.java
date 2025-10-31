88be9f0125c5990651bc535c38cbbd5e75004cdf
Doguparthi Aakash <139471509+DoguparthiAakash@users.noreply.github.com>
2025-10-31 22:01:27 +0530

    Delete src directory

--- src/main/java/com/example/notepadpp/editors/SpecializedEditor.java
+++
@@ -1,9 +0,0 @@
-package com.example.notepadpp.editors;
-
-public interface SpecializedEditor {
-    void applySyntaxHighlighting();
-    void applyFormatting();
-    void enableCollaboration();
-    void addComment(String comment);
-    void showVersionHistory();
-}
